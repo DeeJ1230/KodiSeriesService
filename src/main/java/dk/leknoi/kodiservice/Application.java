fe106d3c9ac3ba61647fd6467f428b1ec7925f58
Dan Jorgensen <dan,cph@gmail.com>
2019-06-10 19:51:47 +0200

    Implementing mapper

--- src/main/java/dk/leknoi/kodiservice/Application.java
+++ src/main/java/dk/leknoi/kodiservice/Application.java
@@ -1,10 +1,8 @@
 package dk.leknoi.kodiservice;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -13,8 +11,20 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+//	@Bean
+//	public ModelMapper modelMapper() {
+//		return new ModelMapper();
+//	}
+
+//	@Bean
+	//public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+//	public MappingJacksonHttpMessageConverter jackson2ObjectMapperBuilder() {
+//
+//		return new MappingJacksonHttpMessageConverter()
+//				.propertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE)
+//				.featuresToEnable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
+//				.featuresToEnable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+//				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).modules(new JodaModule())
+//				.serializationInclusion(JsonInclude.Include.NON_EMPTY).defaultUseWrapper(false);
+//	}
 }

8b2233ee9ef7016bf73ef6034cc1eeda1c8c2451
Dan Jorgensen <dan,cph@gmail.com>
2019-06-10 15:41:48 +0200

    Implementing mapper

--- src/main/java/dk/leknoi/kodiservice/service/TvShowService.java
+++ src/main/java/dk/leknoi/kodiservice/service/TvShowService.java
@@ -27,6 +27,6 @@ public class TvShowService implements ITvShowService {
 	@Override
 	public List<TvShow> findSerieByTitel(String titel) {
 		// return serieRepository.findByTitelContaining(titel);
-		return serieRepository.findByC00Containing(titel);
+		return serieRepository.findByShowTitleContaining(titel);
 	}
 }

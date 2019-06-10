3cd8e22311f0fea53a853defa3b843c4f37c43e8
Dan Jorgensen <dan,cph@gmail.com>
2019-06-10 20:11:30 +0200

    Implementing mapper

--- src/main/java/dk/leknoi/kodiservice/Application.java
+++ src/main/java/dk/leknoi/kodiservice/Application.java
@@ -1,14 +1,8 @@
 package dk.leknoi.kodiservice;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -17,14 +11,14 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
-
-		return new Jackson2ObjectMapperBuilder()
-				.propertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE)
-				.featuresToEnable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
-				.featuresToEnable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
-//				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).modules(new JodaModule())
-				.serializationInclusion(JsonInclude.Include.NON_EMPTY).defaultUseWrapper(false);
-	}
+//	@Bean
+//	public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+//
+//		return new Jackson2ObjectMapperBuilder()
+//				.propertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE)
+//				.featuresToEnable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
+//				.featuresToEnable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+////				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).modules(new JodaModule())
+//				.serializationInclusion(JsonInclude.Include.NON_EMPTY).defaultUseWrapper(false);
+//	}
 }

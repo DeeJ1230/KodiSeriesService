5a1f9ad0c25e6c1c299a41d730cedd50e4d98e2a
Dan Jorgensen <dan,cph@gmail.com>
2019-06-12 18:37:11 +0200

    Implementing mapper

--- src/main/java/dk/leknoi/kodiservice/controller/TvshowController.java
+++ src/main/java/dk/leknoi/kodiservice/controller/TvshowController.java
@@ -8,10 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,7 +29,7 @@ public class TvshowController {
 		return serieService.findAll();
 	}
 	
-	@RequestMapping(value="/{idshow}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value="/{idshow}", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ApiResponse<TvShowDto> findSerie(@PathVariable("idshow") int idShow) {
 
 		TvShow tvShow = serieService.findSerieByIdshow(idShow);
